aacc25a347fc6f5a2d1949fa0f68100c2625d1ab
DanielDiazDev <danieldiazdev00@gmail.com>
2023-01-20 18:11:02 -0300

    Create enemies and their movements

---
+++ Assets/Scripts/Tower/EnemyDetector.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+namespace Tower
+{
+    public class EnemyDetector : MonoBehaviour
+    {
+        public float rangeDistance;
+        public float fireRate;
+        private Transform _target;
+        private Vector3 _closestEnemyRef;
+        public Vector2 Direction { get; private set; }
+        private bool _detected = false;
+        private float _nextTimeToFire = 0;
+        private GameObject[] _enemies;
+        [SerializeField] private Shooting _shooting;
+
+        private void Start()
+        {
+            _enemies = GameObject.FindGameObjectsWithTag("Unit");
+            InvokeRepeating("UpdateTarget", 0.0f, 0.5f);
+        }
+
+        private void Update()
+        {
+            if (_target != null)
+            {
+                Direction = _target.transform.position - transform.position;
+                RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, rangeDistance);
+                if (rayInfo)
+                {
+                    if (rayInfo.collider.gameObject.CompareTag("Unit"))
+                    {
+                        if (_detected == false)
+                        {
+                            _detected = true;
+                        }
+                    }
+                    else
+                    {
+                        if (_detected == true)
+                        {
+                            _detected = false;
+                        }
+                    }
+                }
+                if (_detected)
+                {
+                    if (Time.time > _nextTimeToFire)
+                    {
+                        _nextTimeToFire = Time.time + 1 / fireRate;
+                        _shooting.Shoot();
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // Draw View Distance
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, rangeDistance);
+            // Draw View Draw collision Area
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, .25f);
+            // Draw View Draw Enemy distance from player
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.position, _closestEnemyRef);
+        }
+        private void UpdateTarget()
+        {
+            float shortestDistance = Mathf.Infinity;
+            GameObject closestEnemy = null;
+
+            foreach (GameObject currentEnemy in _enemies)
+            {
+                if (currentEnemy != null)
+                {
+                    float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+                    if (distanceToEnemy < shortestDistance)
+                    {
+                        shortestDistance = distanceToEnemy;
+                        closestEnemy = currentEnemy;
+                        _closestEnemyRef = currentEnemy.transform.position;
+                    }
+                }
+                
+            }
+            // Check that the enemy that is closer to the turret became the main target
+            if (closestEnemy != null && shortestDistance <= rangeDistance)
+            {
+                _target = closestEnemy.transform;
+            }
+            else
+            {
+                _target = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Unit/AIMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Tower;
+using UnityEngine;
+
+namespace Unit
+{
+    public class AIMovement : MonoBehaviour
+    {
+        [SerializeField] private GameObject _player;
+        public float speed;
+        private float _distance;
+        private float _angle;
+        private Vector2 _direction;
+        private HealthSystem _healthSystem;
+
+        private void Start()
+        {
+            _healthSystem = GameObject.FindGameObjectWithTag("Tower").GetComponent<HealthSystem>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _distance = Vector2.Distance(transform.position, _player.transform.position);
+            _direction = _player.transform.position - transform.position;
+            _direction.Normalize();
+            _angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+            Move();
+            ClosePlayer();
+            
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawLine(transform.position, _player.transform.position);
+        }
+        void Move()
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, _player.transform.position,
+                speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * _angle);
+        }
+        void ClosePlayer()
+        {
+            float distanceToPlayer = (_player.transform.position - this.transform.position).sqrMagnitude;
+            if(distanceToPlayer <= 0.12f)
+            {
+               _healthSystem.TakeDamage(1);
+               Destroy(gameObject);
+            }
+        }
+    }
+}
